2d0e50f9bd98542ff67df45fc445eaadf0959069
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-08-26 10:19:49 +0800

    refactor

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import Container from './src/components/navigation/routes';
-
-
+import React from "react";
+import Container from "./src/components/navigation/routes";
 
 export default class App extends React.Component {
-    render() {
-      return <Container />;
-    }
+  render() {
+    return <Container />;
+  }
 }
